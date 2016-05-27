dd015c46f62c596a97e103975639c9686dd3fca8
tracyredcross <tracyredcross@gmail.com>
2016-05-27 17:11:08 -0400

    Update cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -2,6 +2,7 @@
 ## functions do
 
 ## Write a short comment describing this function
+## assigns object a value different from current value
 
 makeCacheMatrix <- function(x = matrix()) {
 
@@ -9,6 +10,7 @@ makeCacheMatrix <- function(x = matrix()) {
 
 
 ## Write a short comment describing this function
+## calculates the mean from the data then sets the mean value in cache via setmean .
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
